e0da322726e03e54c9e77460eeeaa72d9778353f
Mork <anon@users.noreply.github.com>
2019-03-19 11:53:42 +0000

    specified gas price (oops;)

--- truffle-config.js
+++ truffle-config.js
@@ -68,6 +68,7 @@ module.exports= {
       provider: () => new HDWalletProvider(secrets.mnemonic, `https://ropsten.infura.io/${secrets.projectSecret}`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      gasPrice: 2000000000, // 2 GWei
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
